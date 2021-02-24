f3b1c990c6655b9ed420c47eb3b0a5a8884bf75d
Krishna Venkatramani <krishsgithub1@gmail.com>
2021-02-24 16:20:31 +0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useSpring,animated} from 'react-spring';
+import Toggle from './Toggle'
+import Menu from './Menu'
+import Checkout from './Checkout'
+const App = () => {
+  let menuRef = useRef(null)
+  let checkoutRef = useRef(null)
+  const fade = useSpring({
+    from: {
+      opacity: 0
+    },
+    to:{
+      opacity: 1
+    }
+  });
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="logo" />
-          <button className="menu-button">Menu</button>
-        </header>
-      </div>
-    );
+  var onClick = (button) => {
+    var container = null
+    switch(button){
+      case 'menu':
+        container = menuRef.current
+        break;
+      case 'checkout':
+        container = checkoutRef.current
+        break;
+      default:
+        console.log('Container will be empty')
+        break;
+    }
+    // console.log('Menu button was clicked!');
+    // if (menuRef.current){
+    //   console.log('menu should open now !')
+    //   menuRef.current.clickOpen()
+    // }
+    if(container){
+      container.clickOpen()
+    }
   }
-}
 
+  var hideToggle = () => menuRef && menuRef.current && menuRef.current.openMenu ? 0 : 1 || 1
+  return (
+    <animated.div className="App" style={fade}>
+      <header className="App-header">
+        <img src={logo} className="logo" />
+        <button className="menu-button" onClick={() => {onClick('menu')}}>Menu</button>
+        <button className="menu-button" onClick={() => {onClick('checkout')}}>Checkout</button>
+      </header>
+      <Menu ref={menuRef}/>
+      <Checkout ref={checkoutRef}/>
+      <Toggle style={{opacity: hideToggle()}}/>
+    </animated.div>
+  );
+}
 export default App;
